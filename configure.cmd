cef60998b58790e55f432e155829f61ddff89b7d
KO Myung-Hun <komh@chollian.net>
2012-02-12 15:45:04 +0900

    Disable SDL for an output device

--- configure.cmd
+++ configure.cmd
@@ -6,5 +6,6 @@ extproc sh
             --enable-os2threads \
             --disable-pthreads \
             --enable-runtime-cpudetect \
+            --disable-outdev=sdl \
             --extra-cflags="-D_GNU_SOURCE -DCONFIG_FASTMEMCPY=1" \
             $@
